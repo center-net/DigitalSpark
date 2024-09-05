77d4d66d7c65a514ebdab26ab57181660c235f8a
mrpya2007 <mrpya2007@gmail.com>
2024-09-06 00:29:31 +0300

    add translatable

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -50,6 +50,15 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                <ul>
+                    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        <li>
+                            <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                                {{ $properties['native'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
 
             <!-- Hamburger -->
